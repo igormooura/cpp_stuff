e10b39cfaab0d1f6f07db1cb8dcabb75f496e516
igormooura <igor.oliveira.moura@gmail.com>
2025-09-02 21:44:00 -0300

    faster and commented (pt-br)

--- leetcodes/mergesort/912.cpp
+++ leetcodes/mergesort/912.cpp
@@ -2,22 +2,31 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> sortArray(vector<int>& nums) {
+    vector<int> sortArray(vector<int> &nums)
+    {
         int middle = nums.size() / 2;
 
-        if (nums.size() > 1) {
+        if (nums.size() > 1)
+        {
             vector<int> v1(middle);
             vector<int> v2(nums.size() - middle);
 
-            for (int i = 0; i < middle; i++) {
+
+            // colocar os trem nos vetores
+            for (int i = 0; i < middle; i++)
+            {
                 v1[i] = nums[i];
             }
-            for (int j = middle; j < nums.size(); j++) {
+
+            for (int j = middle; j < nums.size(); j++)
+            {
                 v2[j - middle] = nums[j];
             }
 
+            // ordena cada metade recursivamente
             v1 = sortArray(v1);
             v2 = sortArray(v2);
 
@@ -27,40 +36,48 @@ public:
     }
 
 private:
-    vector<int> merge(vector<int>& left, vector<int>& right) {
-        vector<int> result;
+    vector<int> merge(vector<int> &left, vector<int> &right)
+    {
+        vector<int> result(left.size() + right.size());
 
-        int i = 0;
-        int j = 0;
+        int i = 0, j = 0, k = 0; // i = left, j = right
 
-        while (i < left.size() && j < right.size()) {
-            if (left[i] <= right[j]) {
-                result.push_back(left[i++]);
-            } else {
-                result.push_back(right[j++]);
+
+        // intercala elementos dos 2 vetores enquanto eles tem elementos
+        while (i < left.size() && j < right.size())
+        {
+            if (left[i] <= right[j])
+            {
+                result[k++] = left[i++];
+            }
+            else
+            {
+                result[k++] = right[j++];
             }
         }
 
-        while (i < left.size()) {
-            result.push_back(left[i++]);
+        //adiciona os elementos restantes, no caso o haja diferença entre right e left
+
+        while (i < left.size()){ 
+            result[k++] = left[i++];
         }
-        while (j < right.size()) {
-            result.push_back(right[j++]);
+        while (j < right.size()){ 
+            result[k++] = right[j++];
         }
 
         return result;
     }
 };
 
-int main() {
+int main()
+{
     Solution s;
     vector<int> nums = {5, 2, 3, 1, 4};
     vector<int> sorted = s.sortArray(nums);
 
     cout << "Vetor ordenado: ";
-    for (int n : sorted) {
+    for (int n : sorted)
         cout << n << " ";
-    }
     cout << endl;
 
     return 0;
