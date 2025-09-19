3b889ec7f0b2fd9849a64c802d8651d39fd03826
Igor Moura <igor.oliveira.moura@gmail.com>
2025-09-18 23:06:49 -0300

    good notes

--- leetcodes/stack/2000.cpp
+++ leetcodes/stack/2000.cpp
@@ -7,20 +7,21 @@ public:
     string reversePrefix(string word, char ch) {
 
         stack<char> p1;
-        int idx = -1; // indica se o caractere foi encontrado IDEIA DO CHATGPT MEMO
+        int idx = -1; 
 
         for (int i = 0; i < word.size(); i++) {
             p1.push(word[i]);
             if (word[i] == ch) {
-                idx = i; 
+                idx = i; // pra indicar se achou
                 break;
             }
         }
 
-        if (idx == -1) return word; // se não encontrou, retorna original
+        if (idx == -1) return word; 
 
+         // desempilha e sobrescreve a string até o índice idx
         for (int i = 0; i <= idx; i++) {
-            word[i] = p1.top();
+            word[i] = p1.top(); // usa e descarta a letra
             p1.pop();
         }
 
