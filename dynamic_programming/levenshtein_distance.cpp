3ac3ab167a5df4547cb554204d4c70a071fad83b
Igor Moura <igor.oliveira.moura@gmail.com>
2026-05-18 10:39:49 -0300

    distancia de edição

---
+++ dynamic_programming/levenshtein_distance.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+
+    cin >> n >> m;
+
+    string a1, a2;
+
+    cin >> a1 >> a2;
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+
+    for (int i = 0; i <= n; i++) dp[i][0] = i;
+    for (int j = 0; j <= m; j++) dp[0][j] = j;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (a1[i - 1] == a2[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else
+                dp[i][j] = 1 + min({dp[i - 1][j],
+                                    dp[i][j - 1],
+                                    dp[i - 1][j - 1]});
+        }
+    }
+
+    cout << dp[n][m] << endl;
+    return 0;
+}
