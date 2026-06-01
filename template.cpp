66f0e929785ebd08f4b351c0624a982ef5b7bfb5
Igor Moura <igor.oliveira.moura@gmail.com>
2026-05-31 22:04:46 -0300

    template

--- template.cpp
+++ template.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
+
+#define endl '\n'
+#define MOD 1e9+7
+
 using namespace std;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using ll = long long;
+using tiii = tuple<int, int, int>;
 
-int main() {
+void solve()
+{
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     
-
     return 0;
 }
